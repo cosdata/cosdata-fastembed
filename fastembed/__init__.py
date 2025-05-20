e1934157ba851bf40fa444fc1b6f75698b9e2e45
jackson-laskey <jackson.laskey@gmail.com>
2025-05-20 16:32:59 +0800

    Update version + __init__.py

--- fastembed/__init__.py
+++ fastembed/__init__.py
@@ -7,9 +7,9 @@ from fastembed.sparse import SparseEmbedding, SparseTextEmbedding
 from fastembed.text import TextEmbedding
 
 try:
-    version = importlib.metadata.version("fastembed")
+    version = importlib.metadata.version("cosdata-fastembed")
 except importlib.metadata.PackageNotFoundError as _:
-    version = importlib.metadata.version("fastembed-gpu")
+    version = importlib.metadata.version("cosdata-fastembed-gpu")
 
 __version__ = version
 __all__ = [
